c6aa669d57ac6a132da3c5554a543724532cb7a7
Sahil <sahileng03@gmail.com>
2023-02-18 19:39:55 +0530

    tried to connect multiple images

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-const panoramaImage = new PANOLENS.ImagePanorama("images/cf_middle.jpg");
+let panorama;
 const imageContainer = document.querySelector(".image-container");
+const path = 'icons/arrow-upward';
+
+const format = '.png'
+
+panorama = new PANOLENS.CubePanorama( [
+    path + format, path + format,
+    path + format, path + format,
+    path + format, path + format
+  ] )
+
+let panorama2 = new PANOLENS.ImagePanorama('images/cf_outside.jpg')
+
+// panorama2 = new PANOLENS.CubePanorama( [
+//   path + format, path + format,
+//   path + format, path + format,
+//   path + format, path + format
+// ] )
+
+let panorama3 = new PANOLENS.ImagePanorama('images/dental_campus_entrance.jpg')
+
+// panorama3 = new PANOLENS.CubePanorama( [
+//   path + format, path + format,
+//   path + format, path + format,
+//   path + format, path + format
+// ] )
+
+panorama.link(panorama2, new THREE.Vector3( -807.50, 604.88, -5000.00))
+panorama2.link(panorama3, new THREE.Vector3( -807.50, 604.88, -5000.00))
+panorama3.link(panorama, new THREE.Vector3( -807.50, 604.88, -5000.00))
 
 const viewer = new PANOLENS.Viewer({
   container: imageContainer,
@@ -8,4 +37,9 @@ const viewer = new PANOLENS.Viewer({
   controlBar: false,
 });
 
-viewer.add(panoramaImage);
+viewer.add(panorama, panorama2, panorama3)
+
+
+
+
+
