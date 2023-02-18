63b716dbdb7e0fdd8a5945b18690b5340089e83d
MeetBoda <96529606+MeetBoda@users.noreply.github.com>
2023-02-18 15:52:36 +0530

    main.js updated

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const panoramaImage = new PANOLENS.ImagePanorama("images/main_campus_entrance.jpg");
+const panoramaImage = new PANOLENS.ImagePanorama("images/cf_middle.jpg");
 const imageContainer = document.querySelector(".image-container");
 
 const viewer = new PANOLENS.Viewer({
