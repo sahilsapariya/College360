fef052fac4bdcfd3b58fc67eb77a3d62fae63c27
Sahil <sahileng03@gmail.com>
2023-02-19 05:04:26 +0530

    made some changes in index.html and main.js file and added some styling

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let panorama;
+let panorama, panorama2, panorama3, viewer;
 const imageContainer = document.querySelector(".image-container");
-const path = 'icons/arrow-upward';
 
+const path = 'icons/arrow-upward';
 const format = '.png'
 
 panorama = new PANOLENS.CubePanorama( [
@@ -10,31 +10,20 @@ panorama = new PANOLENS.CubePanorama( [
     path + format, path + format
   ] )
 
-let panorama2 = new PANOLENS.ImagePanorama('images/cf_outside.jpg')
+panorama2 = new PANOLENS.ImagePanorama('images/cf_outside.jpg')
 
-// panorama2 = new PANOLENS.CubePanorama( [
-//   path + format, path + format,
-//   path + format, path + format,
-//   path + format, path + format
-// ] )
+panorama3 = new PANOLENS.ImagePanorama('images/dental_campus_entrance.jpg')
 
-let panorama3 = new PANOLENS.ImagePanorama('images/dental_campus_entrance.jpg')
 
-// panorama3 = new PANOLENS.CubePanorama( [
-//   path + format, path + format,
-//   path + format, path + format,
-//   path + format, path + format
-// ] )
+panorama.link(panorama2, new THREE.Vector3( -1007.50, 404.88, -5000.00))
+panorama2.link(panorama3, new THREE.Vector3( 50.50, -400.88, -5000.00))
+panorama3.link(panorama, new THREE.Vector3( 50.50, -400.88, -5000.00))
 
-panorama.link(panorama2, new THREE.Vector3( -807.50, 604.88, -5000.00))
-panorama2.link(panorama3, new THREE.Vector3( -807.50, 604.88, -5000.00))
-panorama3.link(panorama, new THREE.Vector3( -807.50, 604.88, -5000.00))
-
-const viewer = new PANOLENS.Viewer({
+viewer = new PANOLENS.Viewer({
   container: imageContainer,
   // autoRotate: true,
 //   autoRotateSpeed: 0.8,
-  controlBar: false,
+  // controlBar: false,
 });
 
 viewer.add(panorama, panorama2, panorama3)
